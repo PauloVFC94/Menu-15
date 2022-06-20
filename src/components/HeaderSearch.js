800ebd17de67e9c5aa4cbd1f98ca56e83fe76c8d
Matheus Monteiro <matheuschran@hotmail.com>
2022-06-20 18:39:02 -0300

    [tests] cria arquivo renderWithRouter [tests] - cria testes unitários requisito 13 [feature] - cria inputs no componente HeaderSearch

--- src/components/HeaderSearch.js
+++ src/components/HeaderSearch.js
@@ -2,9 +2,46 @@ import React from 'react';
 
 function HeaderSearch() {
   return (
-    <h1>
-      hello world
-    </h1>
+    <span className="search-header">
+      <input
+        type="text"
+        data-testid="search-input"
+        placeholder="Search Recipe"
+      />
+      <label htmlFor="ingredient-search-radio">
+        Ingredient
+        <input
+          id="ingredient-search-radio"
+          type="radio"
+          name="filterSearch"
+          data-testid="ingredient-search-radio"
+        />
+      </label>
+      <label htmlFor="name-search-radio">
+        Name
+        <input
+          id="name-search-radio"
+          type="radio"
+          name="filterSearch"
+          data-testid="name-search-radio"
+        />
+      </label>
+      <label htmlFor="first-letter-search-radio">
+        First Letter
+        <input
+          id="first-letter-search-radio"
+          type="radio"
+          name="filterSearch"
+          data-testid="first-letter-search-radio"
+        />
+      </label>
+      <button
+        type="button"
+        data-testid="exec-search-btn"
+      >
+        Search
+      </button>
+    </span>
   );
 }
 
