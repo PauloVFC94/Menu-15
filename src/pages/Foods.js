f196d2d67cb6d2b9d09b61028510b19891115567
Matheus Monteiro <matheuschran@hotmail.com>
2022-06-21 10:57:07 -0300

    cria função de requisição à API no componente Foods, completa requisito 14

--- src/pages/Foods.js
+++ src/pages/Foods.js
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import HeaderSearch from '../components/HeaderSearch';
+import RecipesContext from '../context/RecipesContext';
 
 function Foods() {
+  const INITIAL_DATA = {
+    noResults: 'Choose a category or search for a recipe',
+  };
+
+  const { endpoint } = useContext(RecipesContext);
+  const [data, setData] = useState(INITIAL_DATA);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const getData = async () => {
+      if (endpoint !== '') {
+        setLoading(true);
+        try {
+          const response = await fetch(endpoint);
+          const results = await response.json();
+          setData(results.meals);
+          setLoading(false);
+        } catch (error) {
+          setData({
+            noResults: 'Algo deu errado, tente fazer outra busca',
+          });
+          setLoading(false);
+        }
+      }
+    };
+    getData();
+  }, [endpoint]);
+
   return (
     <span>
       <HeaderSearch />
-      <p>i am the food recipes page</p>
+      { loading && <p>Carregando...</p> }
+      { !loading && data.noResults && <p>{ data.noResults }</p> }
     </span>
   );
 }
