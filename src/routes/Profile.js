ac12f27d66c78cfbbe86466ce594f0b63db8a243
estenio <angelojuliorafel@gmail.com>
2022-06-21 17:10:44 -0300

    profile 90%

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,9 +1,47 @@
 import React from 'react';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 function Profile() {
+  const history = useHistory();
+  const emailDoUsuario = 'ALELUIA';
+  console.log(emailDoUsuario);
+
+  const handleClick = () => {
+    console.log('ALELUIA');
+    localStorage.clear();
+    history.push('/');
+  };
+
   return (
-    <Header title="Profile" />
+    <>
+      <Header title="Profile" />
+      <section>
+        <p data-testid="profile-email">{emailDoUsuario}</p>
+        <button
+          type="button"
+          data-testid="profile-done-btn"
+          onClick={ () => { history.push('/done-recipes'); } }
+        >
+          Done Recipes
+        </button>
+        <button
+          type="button"
+          data-testid="profile-favorite-btn"
+          onClick={ () => { history.push('/favorite-recipes'); } }
+        >
+          Favorite Recipes
+        </button>
+        <button
+          type="button"
+          data-testid="profile-logout-btn"
+          onClick={ handleClick }
+        >
+          Logout
+        </button>
+      </section>
+      <Footer />
+    </>
   );
 }
 
