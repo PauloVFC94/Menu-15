581e1d5aaa5d48991dd99f8ff84129926ec67405
Natã A.Rodrigues <nata26ar@gmail.com>
2022-06-24 20:21:28 -0300

    Requisitos 56 e 59 Feitos - Variação do card dependendo do tipo de comida e redirecionamento para a pagina de detalhes

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -1,34 +1,59 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import shareIcon from '../images/shareIcon.svg';
 
 function RecipeCard({ recipe, index }) {
+  const recipeTags = recipe.tags.filter((tag, tagIndex) => tagIndex < 2);
+  const history = useHistory();
+
   return (
     <div data-testid={ `${index}-recipe-card` }>
-      <img
-        src={ recipe.image }
-        alt={ `${recipe.name} thumbnail` }
-        data-testid={ `${index}-horizontal-image` }
-        width="100px"
-      />
+      <button
+        type="button"
+        onClick={ () => history.push(`/${recipe.type}s/${recipe.id}`) }
+      >
+        <img
+          src={ recipe.image }
+          alt={ `${recipe.name} thumbnail` }
+          data-testid={ `${index}-horizontal-image` }
+          width="100px"
+        />
+      </button>
+
       <div>
-        <p data-testid={ `${index}-horizontal-top-text` }>
-          { `${recipe.nationality} - ${recipe.category}` }
-        </p>
+        { recipe.type === 'food'
+          ? (
+            <p data-testid={ `${index}-horizontal-top-text` }>
+              { `${recipe.nationality} - ${recipe.category}` }
+            </p>
+          ) : (
+            <p data-testid={ `${index}-horizontal-top-text` }>
+              { recipe.alcoholicOrNot }
+            </p>
+          ) }
         <img
           src={ shareIcon }
           alt="shareIcon"
           data-testid={ `${index}-horizontal-share-btn` }
         />
       </div>
-      <h5 data-testid={ `${index}-horizontal-name` }>
-        { recipe.name }
-      </h5>
+
+      <button
+        type="button"
+        onClick={ () => history.push(`/${recipe.type}s/${recipe.id}`) }
+      >
+        <h5 data-testid={ `${index}-horizontal-name` }>
+          { recipe.name }
+        </h5>
+      </button>
+
       <p data-testid={ `${index}-horizontal-done-date` }>
         { `Done in: ${recipe.doneDate}` }
       </p>
+
       <div>
-        { recipe.tags.map((tag) => (
+        { recipeTags.map((tag) => (
           <span
             key={ tag }
             data-testid={ `${index}-${tag}-horizontal-tag` }
